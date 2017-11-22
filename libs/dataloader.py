89ded1f1265646a35c0bce18f1ff68f8b779e758
NGnius <ngniusness@gmail.com>
2017-11-22 11:31:22 -0500

    Create data loading module

---
+++ libs/dataloader.py
@@ -0,0 +1,69 @@
+class datafile: # loads and parses files depending on file ending
+    import configparser
+    def loadConfig(self, filename):
+        '''() -> Config class
+        load the config.txt file'''
+        config = configparser.ConfigParser()
+        config.read(filename)
+        return config
+
+    def loadRawText(self, filename):
+        '''() -> list of str
+        returns each file of the line as an element in the list'''
+        file = open(filename, "r")
+        return file.readlines()
+    def saveRawText(self):
+        '''() -> None
+        save file as .txt or extensionless'''
+        if self.type:
+            file = open(self.name+"."+self.type, "w")
+        else:
+            file = open(self.name, "w")
+        text = ""
+        for i in self.content:
+            text+=i+"\n"
+        file.write(text)
+        file.close()
+
+    def loadCSV(self, filename):
+        '''() -> list of list of str
+        splits the file by line and by comma'''
+        contents = loadRawText(filename)
+        contents = [i.split(",") for i in contents] # split at commas
+        contents = [i.strip() for i in contents] # remove whitespaces (in case it was actuall seperated by ", ")
+        return contents
+    def saveCSV(self):
+        '''() -> None
+        saves file as CSV'''
+        file = open(self.name+".csv", "w")
+        text = ""
+        for i in self.content:
+            for j in i:
+                text += j+","
+            text+="\n"
+        file.write(text)
+        file.close()
+
+    def __init__(self, filename):
+        if "." in filename:
+            fileExt = filename[len(filename)-filename[::-1].index("."):].lower()
+            if fileExt == "config":
+                self.content = loadConfig(filename)
+            elif fileExt == "csv":
+                self.content = loadCSV(filename)
+            else:
+                self.content = loadRawText(filename)
+            self.type = fileExt
+            self.name = filename[:len(filename)-filename[::-1].index(".")-1]
+        else:
+            self.content = loadRawText(filename)
+            self.type = None
+            self.name = filename
+    def save():
+        '''() -> None
+        saves the file in the original format it was parsed from, including any chances'''
+        if self.type == "csv":
+            saveCSV()
+        elif self.type != "config":
+            saveRawText()
+        # it's nasty saving Configs
