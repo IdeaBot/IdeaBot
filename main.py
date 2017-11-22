34ab1b4652af8006c21cfd3d83cab9b129005525
NGnius <ngniusness@gmail.com>
2017-11-22 11:31:04 -0500

    Rename Client class

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ sys.path.append('./libs')
 import configloader, scraper
 
 def configureDiscordLogging():
+    '''() -> None
+    set ups discord log so that it outputs to ./discord.log'''
     logger = logging.getLogger('discord')
     logger.setLevel(logging.DEBUG)
     handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
@@ -12,6 +14,8 @@ def configureDiscordLogging():
     logger.addHandler(handler)
 
 def mainLogging():
+    '''() -> Logger class
+    set ups main log so that it outputs to ./main.log and then returns the log'''
     logger = logging.getLogger('main')
     handler = logging.FileHandler(filename='main.log', encoding='utf-8', mode='w')
     handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
@@ -28,10 +32,12 @@ def loadConfig(filename):
 configureDiscordLogging()
 log = mainLogging()
 
-class MyClient(discord.Client): # subClass : overwrites certain functions of discord.CLient class
-
+class DiscordClient(discord.Client): # subClass : overwrites certain functions of discord.CLient class
+    '''To overwrite functions without modifying the Discord API Python code'''
     @asyncio.coroutine
     def on_ready(self):
+        '''() -> None
+        logs info when API is ready'''
         #work or die
         log.info("API connection created successfully")
         log.info("Useranme: " + self.user.name)
@@ -41,6 +47,8 @@ class MyClient(discord.Client): # subClass : overwrites certain functions of dis
 
     @asyncio.coroutine
     def on_message(self, message):
+        '''(Message class) -> None
+        interprets and responds to the message'''
         if message.author != self.user: # everything past here will eventually become some super string parser
             if "hotdog" in message.content.lower() or "dick" in message.content.lower() or "hot-dog" in message.content.lower():
                 yield from self.send_message(message.channel, "Hotdog :)")
@@ -52,10 +60,14 @@ class MyClient(discord.Client): # subClass : overwrites certain functions of dis
                 yield from self.logout()
                 log.info("Shutdown started by: " + message.author.name)
 
+# main
+# init stuff
 loop = asyncio.get_event_loop()
-bot = MyClient()
+bot = DiscordClient()
 credentials = loadConfig("./data/credentials.txt")
 credentials = credentials["DEFAULT"]
 loop.run_until_complete(bot.login(credentials["username"], credentials["password"]))
+
+#run until logged out
 loop.run_until_complete(bot.connect())
 print("Ended")
