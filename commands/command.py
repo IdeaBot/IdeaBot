cec5c5460bfa14de8d2faca76c86a9376c55cb63
Eric Yuhas <lilyuhas@gmail.com>
2018-01-11 21:05:11 -0800

    Command class, with a few helpful subclasses

---
+++ commands/command.py
@@ -0,0 +1,76 @@
+# -*- coding: utf-8 -*-
+"""
+Command is the definition of the command class as well as extensible utilities
+that can be used to create more commands easily.
+
+Created on Thu Jan 11 20:03:56 2018
+
+@author: 14flash
+"""
+
+import time
+
+class Command():
+    """Command represents a command that the discord bot can use to take action
+    based on messages posted in any discord channels it listens to."""
+    
+    def __init__(self, perms=None):
+        # TODO: more verification on the structure of perms
+        self.perms = perms
+    
+    def _matches(self, message):
+        """This function is not intended to be overriden by actual command
+        classes. Classes that wish to provide more utility to sub-classes
+        should use this function to do so."""
+        return message.author.id in self.perms and self.matches(message)
+    
+    def matches(self, message):
+        """This function should be overriden by actual command classes to
+        provide functionality."""
+        return False
+        
+    def _action(self, message, send_func):
+        """This function is not intended to be overriden by actual command
+        classes. Classes that wish to provide more utility to sub-classes
+        should use this function to do so."""
+        return self.action(message, send_func)
+    
+    def action(self, message, send_func):
+        """This function should be overriden by actual command classes to
+        provide functionality."""
+        pass
+
+
+class BenchmarkableCommand(Command):
+    """Extending BenchmarkableCommand will make the bot respond with the time
+    it took to execute a command if "benchmark" appears in the message"""
+    
+    def _action(self, message, send_func):
+        # start the benchmark
+        message_content_lower = message.content.lower()
+        start_time = time.time()
+        # do whatever the classes action is
+        super()._action(message, send_func)
+        # report on benchmark if requested
+        if "benchmark" in message_content_lower:
+            end_time = time.time()
+            yield from send_func(message.channel, "Executed in " + str(end_time-start_time) + " seconds")
+
+
+class DirectOnlyCommand(Command):
+    """Extending DirectOnlyCommand will make the bot only respond to this
+    command if it is mentioned in the message.
+    
+    TODO(14flash): try reworking the structure so that user doesn't have to be passed in"""
+    
+    def __init__(self, perms=None, user=None):
+        super().__init__(perms)
+        # hypothectically you could also make this so that it responds if a 
+        # particular user is @ed, not just the bot
+        if user is None:
+            raise ValueError("DirectOnlyCommand requires a user to be passed in")
+        self.user = user
+    
+    def _matches(self, message):
+        mentioned = self.user.mention in message.content
+        return mentioned and super()._matches(message)
