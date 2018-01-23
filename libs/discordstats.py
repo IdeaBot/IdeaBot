61611eaceee45304b806050cae54966102591847
NGnius <ngniusness@gmail.com>
2018-01-23 15:27:47 -0500

    Clean up file

--- libs/discordstats.py
+++ libs/discordstats.py
@@ -1,37 +1,6 @@
 from libs import dataloader
 import datetime
 
-def runTimeReport(discordClient, interval=3600, filename="./data/msgtimedump.csv"):
-    '''(discord.Client object, int) -> list of [datetime object, int]
-    interval is interpreted as seconds
-    checks messages' times for data analysis purposes '''
-    messages = discordClient.messages
-    now = datetime.datetime.utcnow()
-    results1 = [None]*((round((now - messages[-1].timestamp).total_seconds())//interval)+1)
-    results2 = [None]*((round((now - messages[0].timestamp).total_seconds())//interval)+1)
-    if len(results1) > len(results2):
-        results = results1
-    else:
-        results = results2
-    for i in range(len(results)):
-        results[i] = [now-datetime.timedelta(seconds=(i*interval)%3600, hours=(i*interval)//3600), 0]
-    for i in messages:
-        delta = now - i.timestamp
-        location = int(delta.total_seconds()) // interval
-        results[location][1] += 1
-    results = [[x[0].isoformat(timespec="seconds"), str(x[1])] for x in results]
-    msgInfoFile = dataloader.newdatafile(filename)
-    msgInfoFile.content = results
-    msgInfoFile.save()
-    return results
-
-def dumpMessagesTime(discordClient, filename = "./data/msgdump.csv"):
-    messages = discordClient.messages
-    msgFile = dataloader.newdatafile(filename)
-    for i in messages:
-        msgFile.content.append([i.timestamp.isoformat(timespec="seconds"), i.channel.name]) # yes I'm logging messages' contents right now, don't worry, nobody will see it
-    msgFile.save()
-
 def dumpMessages(discordClient, filename = "./data/msgdump.csv", info = "timestamp.isoformat(timespec='seconds'),channel.name,server.name"):
     '''(discord.Client object, str, str) -> None
     dumps messages' info in discordClient.messages to filename'''
@@ -50,3 +19,37 @@ def dumpMessages(discordClient, filename = "./data/msgdump.csv", info = "timesta
                 msgFile.content[i].append(" ")
         '''
     msgFile.save()
+
+# shortcuts
+
+def dumpMessagesTime(discordClient, filename = "./data/msgdump.csv"):
+    dumpMessages(discordClient, filename=filename, info="timestamp.isoformat(timespec='seconds'),channel.name")
+
+def dumpPlayerActivity(discordClient, filename = "./data/msgdump.csv", info="timestamp.isoformat(timespec='seconds'),author.name, author.id"):
+    dumpMessages(discordClient, filename, info)
+
+# not a shortcut, but still for the lazy
+
+def runTimeReport(discordClient, interval=3600, filename="./data/msgtimedump.csv"):
+    '''(discord.Client object, int) -> list of [datetime object, int]
+    interval is interpreted as seconds
+    analuses messages' times for data analysis purposes '''
+    messages = discordClient.messages
+    now = datetime.datetime.utcnow()
+    results1 = [None]*((round((now - messages[-1].timestamp).total_seconds())//interval)+1)
+    results2 = [None]*((round((now - messages[0].timestamp).total_seconds())//interval)+1)
+    if len(results1) > len(results2):
+        results = results1
+    else:
+        results = results2
+    for i in range(len(results)):
+        results[i] = [now-datetime.timedelta(seconds=(i*interval)%3600, hours=(i*interval)//3600), 0]
+    for i in messages:
+        delta = now - i.timestamp
+        location = int(delta.total_seconds()) // interval
+        results[location][1] += 1
+    results = [[x[0].isoformat(timespec="seconds"), str(x[1])] for x in results]
+    msgInfoFile = dataloader.newdatafile(filename)
+    msgInfoFile.content = results
+    msgInfoFile.save()
+    return result
