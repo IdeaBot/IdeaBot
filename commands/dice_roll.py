04c90cc1bebe100fd155996331017d7f10dbfcdb
Eric Yuhas <lilyuhas@gmail.com>
2018-06-17 17:47:35 -0700

    Multi dice/coin rolls/flips in dice_roll command

--- commands/dice_roll.py
+++ commands/dice_roll.py
@@ -5,19 +5,39 @@ class Command(command.DirectOnlyCommand):
     '''dice roll Command is a command that responds to messages asking for a random number'''
 
     def matches(self, message):
-        return re.search(r'\broll\s(a\s*)?(\d*)\s*(?:-?sided)?\s*(die|dice)?', message.content, re.I) != None or re.search(r'\bflip\s(a\s*)?(\d*)\s*(coin)?', message.content, re.I) != None
+        return self._get_args(message) != None
 
     def action(self, message, send_func):
-        args = re.search(r'\broll\s(a\s*)?(\d*)\s*(?:-?sided)?\s*(die|dice)?', message.content, re.I) or re.search(r'\bflip\s(a\s*)?(\d*)\s*(coin)?', message.content, re.I)
-        if not args.group(2):
-            if args.group(3)=='coin':
-                yield from send_func(message.channel, "Flipped a coin and got %s" % random.choice(["heads", "tails"]))
-            elif args.group(3)=='die' or args.group(3)=='dice':
-                yield from send_func(message.channel, "Rolled a "+args.group(3)+" and got "+str(random.randint(1,6)))
-            else:
-                yield from send_func(message.channel, "Rolled an undetermined sided die and got an undetermined number")
+        args = self._get_args(message)
+        if args.group(1) is None or args.group(2) is None:
+            return
+        if args.group(1) == 'a' or args.group(1) == 'an':
+            num_to_roll = 1
         else:
-            if args.group(3):
-                yield from send_func(message.channel, "Rolled a "+args.group(3)+" and got "+str(random.randint(1,int(args.group(2)))))
-            else:
-                yield from send_func(message.channel, "Rolled and got "+str(random.randint(1,int(args.group(2)))))
+            num_to_roll = int(args.group(1))
+        if args.group(3) == 'coin':
+            # coin has a special words in the response, so placed in separate block
+            flips = []
+            for i in range(num_to_roll):
+                flips.append(random.choice(["heads", "tails"]))
+            yield from send_func(message.channel,
+                                 "Flipped %s coin%s and got %s" % (num_to_roll, ('' if num_to_roll == 1 else 's'), flips))
+            return
+        sides_to_roll = 6
+        if args.group(2) != '':
+            sides_to_roll = int(args.group(2))
+        rolls = []
+        for i in range(num_to_roll):
+            rolls.append(random.randint(1,sides_to_roll))
+        yield from send_func(message.channel,
+                             "Rolled %s di%se and got %s" % (num_to_roll, ('' if num_to_roll == 1 else 'c'), rolls))
+
+    def _get_args(self, message):
+        '''
+        Group 1 is the number of dice/coins to roll/flip.  If it's a roll, group 2
+        is the sides on the dice.
+        '''
+        # roll regex tested at https://regexr.com/3r5n6
+        # There is one case where this regex erros: roll # sided die will roll # 6-sided dice instead of 1 #-sided dice
+        # flip regex tested at https://regexr.com/3r5nc
+        return re.search(r'\broll\s+(an?|\d+(?!-))?\s*(\d*)\s?(?:-?sided)?\s*(die|dice)?', message.content, re.I) or re.search(r'\bflip\s+(a|\d+)()\s+(coin)?', message.content, re.I)
