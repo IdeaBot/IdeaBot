791e850e26f256c6a09e0ad268c6c97856db578a
NGnius <ngniusness@gmail.com>
2018-08-01 22:19:46 -0400

    Flesh out status system

--- libs/discordstats.py
+++ libs/discordstats.py
@@ -1,58 +1,13 @@
 from libs import dataloader
 import datetime
 
-def dumpMessages(discordClient, filename = "./data/msgdump.csv", info = "timestamp.isoformat(timespec='seconds'),channel.name,server.name"):
-    '''(discord.Client object, str, str) -> None
-    dumps messages' info in discordClient.messages to filename'''
-    global i, messages # horrible idea but it works
-    messages = list(discordClient.messages)
-    msgFile = dataloader.newdatafile(filename)
-    infostrip = info.split(",")
-    msgFile.content = [None]*len(messages)
-    for i in range(len(messages)):
-        msgFile.content[i] = [eval("messages[i]."+x) for x in infostrip]
-        '''
-        for j in info:
-            try:
-                msgFile.content[i].append(eval("messages[i]."+j))
-            except:
-                msgFile.content[i].append(" ")
-        '''
-    msgFile.save()
-
-# shortcuts
+# Dump messages to file
 
 def dumpMessagesTime(discordClient, filename = "./data/msgdump.csv"):
     dumpMessages(discordClient, filename=filename, info="timestamp.isoformat(timespec='seconds'),channel.name")
 
 def dumpPlayerActivity(discordClient, filename = "./data/msgdump.csv", info="timestamp.isoformat(timespec='seconds'),author.name, author.id"):
     dumpMessages(discordClient, filename, info)
-
-# not a shortcut, but still for the lazy
-
-def runTimeReport(discordClient, interval=3600, filename="./data/msgtimedump.csv"):
-    '''(discord.Client object, int) -> list of [datetime object, int]
-    interval is interpreted as seconds
-    analuses messages' times for data analysis purposes '''
-    messages = discordClient.messages
-    now = datetime.datetime.utcnow()
-    results1 = [None]*((round((now - messages[-1].timestamp).total_seconds())//interval)+1)
-    results2 = [None]*((round((now - messages[0].timestamp).total_seconds())//interval)+1)
-    if len(results1) > len(results2):
-        results = results1
-    else:
-        results = results2
-    for i in range(len(results)):
-        results[i] = [now-datetime.timedelta(seconds=(i*interval)%3600, hours=(i*interval)//3600), 0]
-    for i in messages:
-        delta = now - i.timestamp
-        location = int(delta.total_seconds()) // interval
-        results[location][1] += 1
-    results = [[x[0].isoformat(timespec="seconds"), str(x[1])] for x in results]
-    msgInfoFile = dataloader.newdatafile(filename)
-    msgInfoFile.content = results
-    msgInfoFile.save()
-    return results
 
 def dumpMessagesTime(discordClient, filename = "./data/msgdump.csv"):
     messages = discordClient.messages
@@ -85,3 +40,12 @@ def dumpMessages(discordClient, filename = "./data/msgdump.csv", info = "timesta
                 msgFile.content[i].append(" ")
         '''
     msgFile.save()
+
+# convenience functions for live stats
+
+def total_users(discordClient):
+    total = 0
+    for server in discordClient.servers:
+        if not server.unavailable: # handle case where guilds go die in a whole due to Discord server issues
+            total += len(server.members)
+    return total
--- plugins/status.py
+++ plugins/status.py
@@ -1,5 +1,5 @@
-from libs import plugin, embed
-import discord, traceback, datetime
+from libs import plugin, embed, discordstats
+import discord, traceback, datetime, os
 
 CHANNEL = 'channel'
 MESSAGE = 'message'
@@ -12,14 +12,63 @@ class Plugin(plugin.OnReadyPlugin, plugin.AdminPlugin):
         self.channel = discord.Object(id=self.CHANNEL_ID)
         self.message = discord.Object(id=self.MESSAGE_ID)
         self.message.channel = self.channel
+        self.startup_time = datetime.datetime.now()
+        self.weird_restarts = -1
+        self.stats_fileend = 0
+        self.last_message_count = -1
 
     async def action(self):
-        # create embed description
-        description = ""
-        description += "**Servers** : %s \n" %len(self.bot.servers)
-        description += "**Messages seen** : %s \n" %len(self.bot.messages)
-        description += "**Messages watched** : %s \n" %len(self.bot.always_watch_messages)
-        description += "~~Potatoes picked~~ : 2\n"
-        description += "**Last Updated** : %s \n" % datetime.datetime.now().isoformat()
+        try:
+            # used to reset stuff while developing
+            # (edit_message does weird stuff sometimes)
+            # await self.edit_message(self.message, embed=embed.create_embed(description=''))
 
-        await self.edit_message(self.message, new_content=' ', embed=embed.create_embed(description=description))
+            # create embed description
+            description = ""
+            description += "**Servers** : %s \n" %len(self.bot.servers)
+            description += "**Messages seen** : %s \n" %len(self.bot.messages)
+            description += "**Messages watched** : %s \n" %len(self.bot.always_watch_messages)
+            description += "**Users** : %s \n" % discordstats.total_users(self.bot)
+
+            description += " - - - - - - - - - - - - - - - - - - - - - - \n" # seperator
+
+            description += "**Plugins** : %s \n" % len(self.bot.plugins)
+            description += "**Commands** : %s \n" % len(self.bot.commands)
+            description += "**Reactions** : %s \n" % len(self.bot.reactions)
+
+            description += " - - - - - - - - - - - - - - - - - - - - - - \n" # seperator
+
+            description += "**Last saved stats file** : %s \n" % self.stats_fileend
+            description += "**Uptime** : %s \n" % str(datetime.datetime.now()-self.startup_time).split('.')[0] # uptime, without decimal seconds
+            description +="**Weird Restarts** : %s \n" % self.weird_restarts
+            description += "**Idea Size** : %s bytes \n" % get_size()
+            description += "***Last Updated** : %s* \n" % datetime.datetime.now().isoformat()
+            # description += "~~Potatoes picked~~ : 2\n"
+
+            # create embed author
+            author=dict()
+            author['name'] = self.bot.user.name if self.bot.user.name!=None else None
+            author['icon_url'] = self.bot.user.avatar_url
+            author['url'] = r'https://github.com/NGnius/IdeaBot' # Remove if perceived as advertising
+
+            await self.edit_message(self.message, embed=embed.create_embed(description=description, author=author))
+
+            if self.last_message_count>len(self.bot.messages) or self.last_message_count==-1: # detect partial bot restarts
+                self.stats_fileend = self.startup_time.isoformat() # change filename to not save over last file
+                self.weird_restarts += 1
+            discordstats.dumpMessages(self.bot, filename='./data/msgdump%s.csv' % self.stats_fileend)
+            self.last_message_count = len(self.bot.messages)
+        except:
+            traceback.print_exc()
+    '''
+    def shutdown(self): # in order for this to work, shutdown has to be called before the bot terminates it's API connection, which is currently not the case
+        yield from self.edit_message(self.message, new_content=' ', embed=embed.create_embed(description="Offline"))'''
+
+
+def get_size(start_path = '.'):
+    total_size = 0
+    for dirpath, dirnames, filenames in os.walk(start_path):
+        for f in filenames:
+            fp = os.path.join(dirpath, f)
+            total_size += os.path.getsize(fp)
+    return total_size
