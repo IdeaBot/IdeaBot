c34448a939a32f77cad69b4a12f91ce9d8aee45c
NGnius <ngniusness@gmail.com>
2018-08-04 00:26:36 -0400

    Improve notification message design & info

--- plugins/scrapers/scraperyt.py
+++ plugins/scrapers/scraperyt.py
@@ -4,14 +4,18 @@ import requests, json, discord, traceback, math
 
 URL = 'url'
 CHANNEL = 'channel'
-MESSAGE = 'message'
 
 CHANNEL_URL='channel_url'
 UPLOADS_URL='uploads_url'
+UPLOADS_ITEM_URL='upload_items_url'
+VIDEO_URL_START='video_url_start'
+CHANNEL_URL_START='channel_url_start'
 
 # constants for data file (.json)
 SUB_MILESTONE = 'submilestone'
 UPLOAD_COUNT = 'uploads'
+
+YOUTUBE_LOGO = 'https://s.ytimg.com/yts/img/favicon_144-vfliLAfaB.png'
 
 
 class Plugin(plugin.ThreadedPlugin, plugin.OnReadyPlugin):
@@ -22,12 +26,12 @@ class Plugin(plugin.ThreadedPlugin, plugin.OnReadyPlugin):
         if not isinstance(self.data.content, dict):
             self.data.content = dict()
         self.CHANNEL_ID = self.config[CHANNEL]
-        self.MESSAGE_ID = self.config[MESSAGE]
         self.CHANNEL_URL = self.config[CHANNEL_URL]
         self.UPLOADS_URL = self.config[UPLOADS_URL]
+        self.UPLOADS_ITEM_URL = self.config[UPLOADS_ITEM_URL]
+        self.VIDEO_URL_START = self.config[VIDEO_URL_START]
+        self.CHANNEL_URL_START = self.config[CHANNEL_URL_START]
         self.CHANNEL = discord.Object(id=self.CHANNEL_ID) # essentially a discord.Channel object
-        self.MESSAGE = discord.Object(id=self.MESSAGE_ID) # essentially a discord.Message object
-        self.MESSAGE.channel = self.CHANNEL
         self.spawn_process()
 
     def is_new_milestone(self, sub_count):
@@ -53,6 +57,17 @@ class Plugin(plugin.ThreadedPlugin, plugin.OnReadyPlugin):
         else:
             return False
 
+    def get_new_milestone(self, channel_dict):
+        '''(scraperyt Plugin, dict) -> discord.Embed
+        Generates an embed about the subscriber milestone from channel info'''
+
+        channel = channel_dict['items'][0]
+        message = channel['snippet']['title']+" has reached %s subscribers!" % 10**self.data.content[SUB_MILESTONE]
+        footer = {'text':channel['snippet']['title']} # unused
+        author = {'name':channel['snippet']['title'], 'url':self.CHANNEL_URL_START+channel['id'], 'icon_url':YOUTUBE_LOGO}
+        image = {'url':channel['snippet']['thumbnails']['default']['url']}
+        return embed.create_embed(description=message, author=author, image=image)
+
     def new_upload(self, upload_count):
         '''(scraperyt Plugin, int of float or str) -> bool
         returns True if upload_count > old_upload_count; False otherwise
@@ -73,6 +88,19 @@ class Plugin(plugin.ThreadedPlugin, plugin.OnReadyPlugin):
         else:
             return False
 
+    def get_new_upload(self):
+        '''(scraperyt Plugin, dict) -> discord.Embed
+        Generates an embed about the newest upload from channel info'''
+
+        upload_items = json.loads(requests.get(self.UPLOADS_ITEM_URL).text)
+        newest_upload = upload_items['items'][0]
+        title = newest_upload['snippet']['title']
+        description = newest_upload['snippet']['description'].split('\n')[0]
+        image = {'url':newest_upload['snippet']['thumbnails']['maxres']['url']}
+        author = {'name':title, 'url':self.VIDEO_URL_START+newest_upload['snippet']['resourceId']['videoId'], 'icon_url':YOUTUBE_LOGO}
+        footer = {'text':newest_upload['snippet']['channelTitle'], 'icon_url':None}
+        return embed.create_embed(description=description, image=image, author=author, footer=footer)
+
     def threaded_action(self, q):
         '''Keeps a message updated by editing the message periodically'''
         '''This should
@@ -91,14 +119,13 @@ class Plugin(plugin.ThreadedPlugin, plugin.OnReadyPlugin):
             # sub milestone check
             subscriberCount = channel_info['items'][0]['statistics']['subscriberCount']
             if self.is_new_milestone(subscriberCount):
-                message = "IdeaProject has reached %s subscribers!" % 10**self.data.content[SUB_MILESTONE]
-                q.put({plugin.SEND_MESSAGE:{plugin.ARGS:[self.CHANNEL], plugin.KWARGS:{'embed': embed.create_embed(description=message, title="Whoa, we're so popular!")}}})
+                q.put({plugin.SEND_MESSAGE:{plugin.ARGS:[self.CHANNEL], plugin.KWARGS:{'embed': self.get_new_milestone(channel_info) }}})
 
             # new upload check
             videoCount = channel_info['items'][0]['statistics']['videoCount']
             if self.new_upload(videoCount):
                 message = "IdeaProject has uploaded a new video! Go check it out here: %s" % upload_url
-                q.put({plugin.SEND_MESSAGE:{plugin.ARGS:[self.CHANNEL], plugin.KWARGS:{'embed': embed.create_embed(description=message, title="Duuuude, there's a new video to watch!")}}})
+                q.put({plugin.SEND_MESSAGE:{plugin.ARGS:[self.CHANNEL], plugin.KWARGS:{'embed': self.get_new_upload() }}})
 
         except:
             traceback.print_exc()
