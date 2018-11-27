1c3bdd69e1c2fc750ffd7f0ca53d4eb35e583c15
NGnius <ngniusness@gmail.com>
2018-11-27 17:42:55 -0500

    Fix tags bug and fix helpstring

--- commands/setup/help.py
+++ commands/setup/help.py
@@ -4,12 +4,29 @@ from libs import command, embed
 HELP_HELPSTRING = '''
 Use this command to show the usage instructions for a command, reaction-command or plugin (ie any add-on).
 
-**Usage:**
-```@Idea help <add-on>```
-Shows the help information for <add-on>
+**Usage**
+```@Idea help <add-on> [<add-on type>]```
+Where
+**`<add-on>`** is any command/reaction-command/plugin
+**`<add-on type>`** is -c, -r, -l or nothing to indicate whether `<add-on>` is a command/reaction-command/plugin/ambiguous
+*(respectively)*
 
-To get a list of commands/reaction-commands/plugins, use:
-```@Idea list (commands/reactions/plugins)``` (respectively)
+**Example**
+`@Idea help perms_setter`
+
+To see the list of commands/reaction-commands/plugins, use:
+```@Idea list (commands/reactions/plugins)```
+*(respectively)*
+
+If you're really stuck, ask for help from the devs: https://discord.gg/gwq2vS7
+'''
+
+VERBOSE_HELPSTRING = '''Verbosity verboses verb very verbosily'''
+
+MULTIPLE_RESULTS = '''
+Multiple matches for that command found.
+To specify between reaction-command, command and plugin use `-r`, `-c` and `-l`
+*(respectively)*
 '''
 
 class Command(command.AdminCommand, command.DirectOnlyCommand):
@@ -21,8 +38,10 @@ class Command(command.AdminCommand, command.DirectOnlyCommand):
         args = self.collect_args(message)
         response_channel = message.author if '-p' not in message.content else message.channel
 
+        is_verbose = '-v' in message.content
+
         if args == None or not args.group(1):
-            help = self.make_help('', HELP_HELPSTRING)
+            help = self.make_help('', self._help(verbose=is_verbose))
             foot_text = 'Bot maintained by NGnius'
             help.set_footer(text=foot_text)
             yield from send_func(response_channel, embed=help)
@@ -32,25 +51,24 @@ class Command(command.AdminCommand, command.DirectOnlyCommand):
         is_command = args.group(1) in client.commands
         is_plugin = args.group(1) in client.plugins
         is_two = (is_reaction + is_command + is_plugin) > 1
-        has_flag = '-c' in message.content or '-r' in message.content or '-l'
-        is_verbose = '-v' in message.content
+        has_flag = '-c' in message.content or '-r' in message.content or '-l' in message.content
 
         if is_two and not has_flag:
-            yield from send_func(response_channel, 'Please specify between the reaction, command and plugin with `-r`, `-c` and `-l`, respectively')
+            yield from send_func(response_channel, MULTIPLE_RESULTS)
 
-        elif (is_command and not is_two) or ('-c' in message.content and is_reaction and is_command):
+        elif is_command and (not is_two or ('-c' in message.content and is_two)):
             help = self.make_help(args.group(1), client.commands[args.group(1)]._help(verbose=is_verbose))
             foot_text = 'Command maintained by '+(yield from client.get_user_info(self.public_namespace.commanders[self.public_namespace.COMMANDS][args.group(1)][self.public_namespace.OWNER])).name
             help.set_footer(text=foot_text)
             yield from send_func(response_channel, embed=help)
 
-        elif (is_reaction and not is_two) or ('-r' in message.content and is_two):
+        elif is_reaction and (not is_two or ('-r' in message.content and is_two)):
             help = self.make_help(args.group(1), client.reactions[args.group(1)]._help(verbose=is_verbose))
             foot_text = 'Reaction maintained by '+(yield from client.get_user_info(self.public_namespace.commanders[self.public_namespace.REACTIONS][args.group(1)][self.public_namespace.OWNER])).name
             help.set_footer(text=foot_text)
             yield from send_func(response_channel, embed=help)
 
-        elif (is_plugin and not is_two) or ('-l' in message.content and is_two):
+        elif is_plugin and (not is_two or ('-l' in message.content and is_two)):
             help = self.make_help(args.group(1), client.plugins[args.group(1)]._help(verbose=is_verbose))
             foot_text = 'Plugin maintained by '+(yield from client.get_user_info(self.public_namespace.commanders[self.public_namespace.PLUGINS][args.group(1)][self.public_namespace.OWNER])).name
             help.set_footer(text=foot_text)
@@ -60,7 +78,7 @@ class Command(command.AdminCommand, command.DirectOnlyCommand):
             yield from send_func(response_channel, 'Command not found')
 
     def collect_args(self, message):
-        return re.search(r'help\s*(?:me\s)?(?:with\s)?(\S+)?', message.content, re.I)
+        return re.search(r'help(?:\sme)?(?:\swith)?(?:\s([^\-\s]\S*))?', message.content, re.I)
 
     def make_help(self, name, docstring):
         description = '%s Help\n' % name
@@ -68,4 +86,6 @@ class Command(command.AdminCommand, command.DirectOnlyCommand):
         return em
 
     def help(self, *args, verbose=False, **kwargs):
+        if verbose:
+            return VERBOSE_HELPSTRING
         return HELP_HELPSTRING
